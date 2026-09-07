f41fb9fad34644338112ecd91a4b90695bc27668
amit_codeegis2024 <amit.kumar@codeaegis.in>
2026-09-07 18:36:42 +0530

    ch

---
+++ 198_rob.py
@@ -0,0 +1,11 @@
+def rob(nums):
+        prev2 = 0
+        prev1 = 0
+
+        for money in nums:
+            current = max(prev1, money + prev2)
+
+            prev2 = prev1
+            prev1 = current
+
+        return prev1
